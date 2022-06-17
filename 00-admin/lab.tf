26fdce88e77f8dd82a1288192bd7d8c241b19026
Tim Miller <timmil@cisco.com>
2022-06-17 11:58:48 -0700

    Adjust for new student account creation

--- 00-admin/lab.tf
+++ 00-admin/lab.tf
@@ -20,8 +20,8 @@ module "student_orgs_and_roles" {
   source = "./student"
   count  = 15
 
-  student_organization  = "student${count.index + 1}"
-  student_email         = "student${count.index + 1}@${var.student_email_domain}"
+  student_organization  = "student${count.index + 101}"
+  student_email         = "student${count.index + 101}@${var.student_email_domain}"
   server_admin_moid     = local.server_admin_moid
   ucs_domain_admin_moid = local.ucs_domain_admin_moid
   authn_cisco_moid      = local.authn_cisco_moid
